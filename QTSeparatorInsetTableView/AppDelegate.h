48ac4199b634fb25504f7a2f9f98b195f4c75613
tangqi92 <imtangqi@gmail.com>
2017-02-27 14:10:18 +0800

    Initial Commit

---
+++ QTSeparatorInsetTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QTSeparatorInsetTableView
+//
+//  Created by Tang Qi on 27/02/2017.
+//  Copyright © 2017 Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
